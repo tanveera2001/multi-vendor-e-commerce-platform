29316853355c14e4ffb9bf8fb79a81a7590b83b8
Md. Tanveer Ahmed <tanveera2001@gmail.com>
2026-03-26 02:33:26 +0600

    feat(client): add categories carousel section
    - Implemented categories slider using react-multi-carousel
    - Added responsive breakpoints
    - Displayed category items with image and label

---
+++ client/src/components/Categorys.jsx
@@ -0,0 +1,82 @@
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { Link } from "react-router-dom";
+
+const Categorys = () => {
+  const categorys = [
+    "Clothing",
+    "Sports",
+    "Phones",
+    "Laptops",
+    "Monitors",
+    "Tablets",
+    "Audio",
+    "Bags",
+    "Televisions",
+  ];
+
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 6,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 6,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 4,
+    },
+    mdtablet: {
+      breakpoint: { max: 991, min: 464 },
+      items: 4,
+    },
+    mobile: {
+      breakpoint: { max: 768, min: 0 },
+      items: 3,
+    },
+    smmobile: {
+      breakpoint: { max: 640, min: 0 },
+      items: 2,
+    },
+    xsmobile: {
+      breakpoint: { max: 440, min: 0 },
+      items: 1,
+    },
+  };
+  return (
+    <div className="w-full mx-auto relative">
+      <Carousel
+        //autoPlay={true}
+        infinite={true}
+        arrows={true}
+        responsive={responsive}
+        transitionDuration={500}
+      >
+        {categorys.map((c, i) => (
+          <Link
+            className="block p-[2px] border  w-full bg-white aspect-square"
+            key={i}
+            to="#"
+          >
+            <div className="w-full hover:shadow-[1px_3px_19px_1px_rgba(194,188,194,1)]  h-full gap-y-2 flex-col relative  flex justify-center items-center">
+              <img
+                src={`http://localhost:5173/images/admin.png`}
+                className="w-[70%] aspect-square"
+                alt="image"
+              />
+              <div className="bottom-6 w-full mx-auto font-bold left-0 flex justify-center items-center">
+                <span className="py-[2px] px-6 bg-[#3330305d] text-white">
+                  {c}
+                </span>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
+
+export default Categorys;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../components/Banner";
+import Categorys from "../components/Categorys";
 import Header from "../components/Header";
 
 const Home = () => {
@@ -6,6 +7,9 @@ const Home = () => {
     <div className="w-full">
       <Header />
       <Banner />
+      <div className="my-4 max-w-[1440px] mx-auto px-16 sm:px-5 md-lg:px-12 md:px-10">
+        <Categorys />
+      </div>
     </div>
   );
 };
